1b437ddba8dbc4d4ab937309f5588ac91257e366
Michal Szewczak <michaelszewczak@gmail.com>
2017-04-21 14:34:56 +0100

    init commit

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 /* jshint node: true */
 'use strict';
 
+
+var Funnel = require('broccoli-funnel');
+var mergeTrees = require('broccoli-merge-trees');
+var path = require('path');
+
 module.exports = {
-  name: 'ember-cli-openfin-excel-shim'
+  name: 'ember-cli-openfin-excel-shim',
+
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+
+  },
+
+  treeForVendor: function(treeForVendor) {
+    var trees = [];
+    if (vendorTree) {
+      trees.push(vendorTree);
+    }
+    var nmp = path.join(__dirname, 'node_modules');
+    var excelExamplePath = path.dirname(require.resolve('excel-api-example'));
+    trees.push(new Funnel(excelExamplePath, {
+      destDir: 'excel-api-openfin',
+      include: ['ExcelAPI.js'],
+    }));
+    return mergeTrees(trees);
+  }
 };
