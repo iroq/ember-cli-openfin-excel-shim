9eda1be17ce03d7108ef63596cbf40944a90c13a
Michal Szewczak <michal@szewczak.email>
2017-12-07 14:30:19 +0000

    simpler default export

--- addon/index.js
+++ addon/index.js
@@ -1,12 +1,1 @@
-const Excel = window.fin.desktop.Excel;
-
-const openfinProperties = Object.keys(window.fin.desktop);
-const openfinEnabled = !(
-    openfinProperties.length === 1 &&
-    openfinProperties[0] === 'Excel'
-);
-
-export {
-    Excel as default,
-    openfinEnabled
-};
+export default window.fin && window.fin.desktop && window.fin.desktop.Excel;
