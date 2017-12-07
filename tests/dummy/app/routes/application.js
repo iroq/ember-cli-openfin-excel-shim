e28dd8499f2b5d2b160c4f5998fd0e90dbc6951b
Michal Szewczak <michal@szewczak.email>
2017-12-07 14:46:57 +0000

    remove unnecessary route

--- tests/dummy/app/routes/application.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-import Excel, { openfinEnabled } from 'ember-cli-openfin-excel-shim';
-
-export default Ember.Route.extend({
-  afterModel() {
-    window.Excel = Excel;
-    window.openfinEnabled = openfinEnabled;
-  }
-});
